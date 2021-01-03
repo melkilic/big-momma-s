afe2516b6b02c54afe6d0d0bfd51264aa011d3ac
melkilic <melike180400@gmail.com>
2021-01-02 21:18:43 -0600

    proper sizing for the img adjusted, title added

--- src/Hero.js
+++ src/Hero.js
@@ -2,10 +2,12 @@ import './App.css'
 import macarons from './macarons.jpg'
 
 const Hero = () => {
-    // const hero= require('./macarons.jpg')
     return (
       <div className="hero-background">
         <img src={macarons}></img>
+        <div className="text-container">
+        <h1>Big Momma's</h1>
+        </div>
       </div>
     );
 }
